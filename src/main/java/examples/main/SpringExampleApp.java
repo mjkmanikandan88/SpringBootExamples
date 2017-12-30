195ec83a6100bfe6c7e22ed3ecc8b566157992f5
mjkmanikandan88 <mjkmanikandan88@gmail.com>
2017-12-30 16:15:11 +0530

    Initial commit

---
+++ src/main/java/examples/main/SpringExampleApp.java
@@ -0,0 +1,12 @@
+package examples.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleApp.class, args);
+	}
+}
